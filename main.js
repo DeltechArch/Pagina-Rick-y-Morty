4eef7cb918d398479f8a26638c0b219f30d57ac5
Acro <deltecharch@gmail.com>
2023-08-05 23:32:49 -0600

    listo quedo perfecto solo falta analizar

--- main.js
+++ main.js
@@ -3,41 +3,66 @@ const personajesURL = `${BaseURL}/character`;
 
 const personajesContainer = document.getElementById('personajes-container');
 
-// Realizar la solicitud a la API cuando la página se cargue
-document.addEventListener('DOMContentLoaded', () => {
+const obtenerTodosLosPersonajes = () => {
     axios.get(personajesURL)
         .then(response => {
             const personajes = response.data.results;
+            const personajesContainer = document.getElementById('personajes-container');
 
-            // Iterar sobre cada personaje y crear la plantilla
+            // Iterar sobre cada personaje y crear la plantilla HTML
             personajes.forEach(personaje => {
                 const nombre = personaje.name;
                 const imagenURL = personaje.image;
-                const info = `Especie: ${personaje.species}, Género: ${personaje.gender}`;
+                const genero = personaje.gender;
+                const especie = personaje.species;
+                const estado = personaje.status;
+                const ubicacion = personaje.location.name;
+                const origen = personaje.origin.name;
 
                 // Crear el artículo para el personaje
                 const article = document.createElement('article');
-
-                // Crear el contenedor para la imagen
-                const div = document.createElement('div');
 
                 // Crear la imagen
                 const imagen = document.createElement('img');
                 imagen.src = imagenURL;
                 imagen.alt = nombre;
 
-                // Crear los elementos para mostrar el nombre y la info del personaje
+                // Crear el contenedor para el nombre y la información
+                const div = document.createElement('div');
+
+                // Crear el título (nombre del personaje)
                 const h2 = document.createElement('h2');
-                const p = document.createElement('p');
-
                 h2.textContent = nombre;
-                p.textContent = info;
 
-                // Agregar la imagen, el nombre y la info al artículo
-                div.appendChild(imagen);
+                // Crear la descripción con el resto de la información
+                const pDescripcion = document.createElement('p');
+                pDescripcion.textContent = "Descripción:";
+
+                const pGenero = document.createElement('p');
+                pGenero.textContent = "Género: " + genero;
+
+                const pEspecie = document.createElement('p');
+                pEspecie.textContent = "Especie: " + especie;
+
+                const pEstado = document.createElement('p');
+                pEstado.textContent = "Estado: " + estado;
+
+                const pUbicacion = document.createElement('p');
+                pUbicacion.textContent = "Ubicación: " + ubicacion;
+
+                const pOrigen = document.createElement('p');
+                pOrigen.textContent = "Origen: " + origen;
+
+                // Agregar la imagen, el nombre y la descripción al artículo
+                article.appendChild(imagen);
+                div.appendChild(h2);
+                div.appendChild(pDescripcion);
+                div.appendChild(pGenero);
+                div.appendChild(pEspecie);
+                div.appendChild(pEstado);
+                div.appendChild(pUbicacion);
+                div.appendChild(pOrigen);
                 article.appendChild(div);
-                article.appendChild(h2);
-                article.appendChild(p);
 
                 // Agregar el artículo al contenedor de personajes
                 personajesContainer.appendChild(article);
@@ -46,4 +71,19 @@ document.addEventListener('DOMContentLoaded', () => {
         .catch(error => {
             console.error('Error al obtener datos de personajes:', error);
         });
+};
+
+// Realizar la solicitud a la API cuando la página se cargue
+document.addEventListener('DOMContentLoaded', () => {
+    obtenerTodosLosPersonajes();
 });
+
+
+
+
+
+
+
+
+
+
