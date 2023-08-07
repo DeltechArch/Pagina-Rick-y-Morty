52131791aaf764e6142cde18915ac2608a0ea17d
Acro <deltecharch@gmail.com>
2023-08-06 20:19:31 -0600

    Agregando mas contenido  se creacta section de los episodios

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 const BaseURL = "https://rickandmortyapi.com/api";
 const personajesURL = `${BaseURL}/character`;
+const Episodios = `${BaseURL}/episode`;
 
 const personajesContainer = document.getElementById('personajes-container');
+
+axios.get(BaseURL)
+    .then(response =>{
+        const apiAll = response.data;
+        console.log("Esto es lo que hay en la API",apiAll)
+    })
 
 const obtenerTodosLosPersonajes = () => {
     axios.get(personajesURL)
@@ -66,6 +73,8 @@ const obtenerTodosLosPersonajes = () => {
 
                 // Agregar el artículo al contenedor de personajes
                 personajesContainer.appendChild(article);
+
+                //Aqui termina el forEach
             });
         })
         .catch(error => {
@@ -78,12 +87,53 @@ document.addEventListener('DOMContentLoaded', () => {
     obtenerTodosLosPersonajes();
 });
 
+// prube propia
+const obtenerTodosLosEpisodio = () =>{
+    axios.get(Episodios)
+    .then(response => {
+        const info = response.data.results;
+        console.log('Informacion de la api episode :', info);
+
+        const losEpisodiosDeApi = document.getElementById('episodios');
+
+        info.forEach(episodio =>{
+            const alaire = episodio.air_date;
+            const creado = episodio.created;
+            const epi = episodio.episode;
+            const nombre = episodio.name;
 
 
+            const article = document.createElement('article');
+            const informacion = document.createElement('div');
 
 
+            const h1 = document.createElement('h1')
+                h1.textContent = "Nombre: " + nombre;
+            const p = document.createElement('p');
+                p.textContent = "Fecha de aire: " + alaire;
+            const p2 = document.createElement('p');
+                p2.textContent = "Creado: "+ creado;    
+            const p3 = document.createElement('p'); 
+                p3.textContent = "Episodio: "+ epi;
 
+            informacion.appendChild(h1);    
+            informacion.appendChild(p);
+            informacion.appendChild(p2);
+            informacion.appendChild(p3);
 
+            article.appendChild(informacion);
 
+            losEpisodiosDeApi.appendChild(article);
 
+        });
+    })
+    .catch(error => {
+        console.error('no salio:', error);
+    });
 
+};
+
+// Realizar la solicitud a la API cuando la página se cargue
+document.addEventListener('DOMContentLoaded', () => {
+    obtenerTodosLosEpisodio();
+});
